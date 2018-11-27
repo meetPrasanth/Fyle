149ab44585e1dc0541320f453506c199feba131b
Prasanth Venkatesan <prasanth@neurai.io>
2018-11-27 18:37:09 +0530

    [BankInfo] Health check api endpoint implemented

--- src/main/java/com/fyle/fylechallenge/api/FyleServiceResource.java
+++ src/main/java/com/fyle/fylechallenge/api/FyleServiceResource.java
@@ -1,5 +1,16 @@
 package com.fyle.fylechallenge.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bank-info")
 public class FyleServiceResource {
 
+	@GetMapping("/health-check")
+	public String healthCheck() {
+		return "Health Check";
+	}
+	
 }
